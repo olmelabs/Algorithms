39701087b62a9c2d57d0addf0c45e962a8645e7c
Oleg Medyanik <oleg.medyanik@roomex.com>
2020-06-04 10:57:50 +0100

    Added Dijkstra implementation

---
+++ Algorithms/Others/Dijkstra.cs
@@ -0,0 +1,130 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Olmelabs.Algorithms.Others
+{
+    public class Dijkstra
+    {
+        private const int Infinity = int.MaxValue;
+        public class GraphNode
+        {
+            public int Vertex { get; set; }
+            public int Weight { get; set; }
+        }
+
+        public class RouteTableRow
+        {
+            public RouteTableRow(int currentVertex)
+            {
+                Vertex = currentVertex;
+                Distance = Infinity;
+            }
+            public int Vertex { get; set; }
+            public int Distance { get; set; }
+            public int? PrevVertex { get; set; }
+        }
+
+        private readonly List<List<GraphNode>> graph;
+
+        private readonly List<int> visited;
+        private readonly List<int> unvisited;
+
+        private readonly List<RouteTableRow> routes;
+
+        public Dijkstra(List<List<GraphNode>> graph, int startVortex)
+        {
+            this.graph = graph;
+
+            routes = new List<RouteTableRow>(graph.Count);
+
+            unvisited = new List<int>(graph.Count);
+            for (var i = 0; i < graph.Count; i++)
+            {
+                unvisited.Add(i);
+                routes.Add(new RouteTableRow(i));
+            }
+
+            //Start from start node to itself is 0
+            routes.First(r => r.Vertex == startVortex).Distance = 0;
+
+            visited = new List<int>(graph.Count);
+        }
+
+        public List<RouteTableRow> BuildRoutes()
+        {
+            var step = 0;
+            DumpState(step);
+
+            while (unvisited.Count > 0)
+            {
+                //get non-visited vertex with smallest distance
+                var row = routes.Where(r => !visited.Contains(r.Vertex)).OrderBy(r => r.Distance).First();
+
+                //calculate the distance to neighbors
+                foreach (var neighbor in graph[row.Vertex])
+                {
+                    if (visited.Contains(neighbor.Vertex))
+                        continue;
+
+                    //The distance to node the existing cost  plus new route cost:
+                    var distance = routes.First(r => r.Vertex == row.Vertex).Distance + neighbor.Weight;
+
+                    //if the calculated distance is less than our currently-known shortest distance for these neighboring nodes,
+                    //update our tables values with our new "shortest distance"
+                    var neighborRow = routes.First(r => r.Vertex == neighbor.Vertex);
+                    if (distance < neighborRow.Distance)
+                    {
+                        neighborRow.Distance = distance;
+                        neighborRow.PrevVertex = row.Vertex;
+                    }
+                }
+
+                visited.Add(row.Vertex);
+                unvisited.Remove(row.Vertex);
+
+                step++;
+                DumpState(step);
+            }
+
+            return routes;
+        }
+
+        private void DumpState(int step)
+        {
+            Trace.WriteLine($"Step {step}");
+            Trace.WriteLine($"Unvisited: {string.Join(", ", unvisited)}");
+            Trace.WriteLine($"Visited: {string.Join(",", visited)}");
+            Trace.WriteLine(" Vtx | Dist | Prev");
+            foreach (var row in routes)
+            {
+                Trace.WriteLine($" {row.Vertex}   | {(row.Distance == Infinity ? "-" : row.Distance.ToString())}    | {row.PrevVertex}");
+            }
+        }
+
+        public List<int> GetShortestPath(int fromVertex, int toVertex)
+        {
+            var s = new Stack<int>();
+            var curr = toVertex;
+
+            while (curr != fromVertex)
+            {
+                s.Push(curr);
+                curr = routes.First(r => r.Vertex == curr).PrevVertex ?? throw new Exception();
+            }
+            s.Push(curr);
+
+            var res = new List<int>(s.Count);
+            while (s.Count > 0)
+            {
+                res.Add(s.Pop());
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTests/DijkstraTests.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Olmelabs.Algorithms.Others;
+
+namespace Olmelabs.Algorithms.UnitTests
+{
+    [TestClass]
+    public class DijkstraTests
+    {
+        [TestMethod]
+        public void Test_Vertex_0()
+        {
+            var g = GetGraph();
+
+            var d = new Dijkstra(g, 0);
+            d.BuildRoutes();
+
+            var path1 = d.GetShortestPath(0, 1);
+            var path2 = d.GetShortestPath(0, 2);
+            var path3 = d.GetShortestPath(0, 3);
+            var path4 = d.GetShortestPath(0, 4);
+            Trace.WriteLine(string.Join(" - ", path1.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path2.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path3.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path4.ToArray()));
+
+        }
+
+        [TestMethod]
+        public void Test_Vertex_2()
+        {
+            var g = GetGraph();
+
+            var d = new Dijkstra(g, 2);
+            d.BuildRoutes();
+
+            var path1 = d.GetShortestPath(2, 0);
+            var path2 = d.GetShortestPath(2, 1);
+            var path3 = d.GetShortestPath(2, 3);
+            var path4 = d.GetShortestPath(2, 4);
+            Trace.WriteLine(string.Join(" - ", path1.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path2.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path3.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path4.ToArray()));
+
+        }
+
+        [TestMethod]
+        public void Test_Vertex_4()
+        {
+            var g = GetGraph();
+
+            var d = new Dijkstra(g, 4);
+            d.BuildRoutes();
+
+            var path1 = d.GetShortestPath(4, 0);
+            var path2 = d.GetShortestPath(4, 1);
+            var path3 = d.GetShortestPath(4, 2);
+            var path4 = d.GetShortestPath(4, 3);
+            Trace.WriteLine(string.Join(" - ", path1.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path2.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path3.ToArray()));
+            Trace.WriteLine(string.Join(" - ", path4.ToArray()));
+
+        }
+
+
+        private List<List<Dijkstra.GraphNode>> GetGraph()
+        {
+            //this graph https://miro.medium.com/max/1400/1*DcCsQRtkOCusXQFodZ7IhA.jpeg
+            return new List<List<Dijkstra.GraphNode>>
+            {
+                //node 0 - a
+                new List<Dijkstra.GraphNode>(new[]
+                {
+                    new Dijkstra.GraphNode(){Vertex = 1, Weight = 7},
+                    new Dijkstra.GraphNode(){Vertex = 2, Weight = 3}
+                }),
+
+                //node 1 - b
+                new List<Dijkstra.GraphNode>(new[]
+                {
+                    new Dijkstra.GraphNode(){Vertex = 0, Weight = 7},
+                    new Dijkstra.GraphNode(){Vertex = 2, Weight = 1},
+                    new Dijkstra.GraphNode(){Vertex = 3, Weight = 2},
+                    new Dijkstra.GraphNode(){Vertex = 4, Weight = 6}
+                }),
+
+                //node 2 - c
+                new List<Dijkstra.GraphNode>(new[]
+                {
+                    new Dijkstra.GraphNode(){Vertex = 0, Weight = 3},
+                    new Dijkstra.GraphNode(){Vertex = 1, Weight = 1},
+                    new Dijkstra.GraphNode(){Vertex = 3, Weight = 2}
+                }),
+
+                //node 3 - d
+                new List<Dijkstra.GraphNode>(new[]
+                {
+                    new Dijkstra.GraphNode(){Vertex = 1, Weight = 2},
+                    new Dijkstra.GraphNode(){Vertex = 2, Weight = 2},
+                    new Dijkstra.GraphNode(){Vertex = 4, Weight = 4},
+                }),
+
+                //node 4 - e
+                new List<Dijkstra.GraphNode>(new[]
+                {
+                    new Dijkstra.GraphNode(){Vertex = 1, Weight = 6},
+                    new Dijkstra.GraphNode(){Vertex = 3, Weight = 4},
+                })
+            };
+        }
+    }
+}
